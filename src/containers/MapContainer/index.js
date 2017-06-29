5eb892fb02d626a6be710d99976bcd6f56ed704c
Zac Baston <zbaston@gmail.com>
2017-06-29 13:39:54 -0400

    add basic onClick

--- src/containers/MapContainer/index.js
+++ src/containers/MapContainer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { GeoJSONLayer } from "react-mapbox-gl";
+import ReactMapboxGl, { GeoJSONLayer, ZoomControl } from "react-mapbox-gl";
 import * as MapboxGL from 'mapbox-gl';
 
 const geojson = require('../../data/neighborhoods.json');
@@ -10,13 +10,25 @@ const fillPaintStyle: MapboxGL.FillPaint = {
   'fill-color': '#ff8787',
   'fill-opacity': 0.5,
   'fill-antialias': true,
-  'fill-outline-color': '#2F1616'
+  'fill-outline-color': '#2F1616',
 };
 
+// const symbolLayoutStyle: MapboxGL.SymbolLayout = {
+//   'text-field': '{nhood}',
+//   'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+//   'text-offset': [0, 0.6],
+//   'text-anchor': 'top'
+// }
 
 class MapContainer extends Component {
   constructor(props) {
     super(props)
+
+    this.onNeighborhoodClick = this.onNeighborhoodClick.bind(this);
+  }
+
+  onNeighborhoodClick(e) {
+    console.log(e)
   }
 
   render(){
@@ -29,7 +41,9 @@ class MapContainer extends Component {
         <Map
           style="mapbox://styles/mapbox/dark-v9"
           center={[-122.3321, 47.6062]}
-          containerStyle={mapStyle}>
+          containerStyle={mapStyle}
+          onClick={this.onNeighborhoodClick}>
+            <ZoomControl />
             <GeoJSONLayer
               data={geojson}
               fillPaint={fillPaintStyle}
